2938f805ac41b5cef3a3a95a29158a04d26aa0e4
ivanburda1986 <burda.ivan@gmail.com>
2021-04-13 23:17:05 +0200

    creating store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+
 import App from '../src/components/App';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux'; 
+
 import reportWebVitals from './reportWebVitals';
+
+
+const store = createStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
