8e292f8cbc8eae23a72d4e8abfff0c723ab281c0
ivanburda1986 <burda.ivan@gmail.com>
2021-04-25 01:02:03 +0200

    implement dark-light theme

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
 
+import {ThemeProvider} from './contexts/theme';
+
 import './index.module.css';
 import classes from './index.module.css';
 
@@ -11,24 +13,34 @@ import StandaloneStory from './components/StandaloneStory/StandaloneStory';
 import User from './components/User/User';
 
 class App extends React.Component{
+  state = {
+    theme: 'light',
+    toggleTheme: () => {
+      this.setState(({theme})=>({
+        theme: theme === 'light' ? 'dark' : 'light'
+      }))
+    }
+  }
   render(){
 
     return(
       <React.Fragment>
         <React.Suspense>
-          <BrowserRouter>
-          <div className={classes.wrapper}>
-            <Navigation/>
-            <Switch>
-                <Route path="/" exact render={(props)=><Stories storyType="top"/>}/>
-                <Route path="/new" exact render={(props)=><Stories storyType="new"/>}/>
-                <Route path="/story" component={StandaloneStory}/>
-                <Route path="/user" component={User}/>
-                {/* This is a 404 fallback which redirects to the main route */}
-                <Redirect to="/"/>
-            </Switch>
-          </div>
-          </BrowserRouter>
+          <ThemeProvider value = {this.state}>
+            <BrowserRouter>
+            <div className={classes.wrapper}>
+              <Navigation/>
+              <Switch>
+                  <Route path="/" exact render={(props)=><Stories storyType="top"/>}/>
+                  <Route path="/new" exact render={(props)=><Stories storyType="new"/>}/>
+                  <Route path="/story" component={StandaloneStory}/>
+                  <Route path="/user" component={User}/>
+                  {/* This is a 404 fallback which redirects to the main route */}
+                  <Redirect to="/"/>
+              </Switch>
+            </div>
+            </BrowserRouter>
+          </ThemeProvider>
         </React.Suspense>
       </React.Fragment> 
     )
