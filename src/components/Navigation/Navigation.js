b16c1158aa64d0414d13e95a3506c3380c774722
ivanburda1986 <burda.ivan@gmail.com>
2021-04-27 00:37:12 +0200

    improve navlink colors

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,17 +2,21 @@ import React from 'react';
 import {NavLink} from 'react-router-dom';
 import {ThemeConsumer} from '../../contexts/theme';
 import classes from './Navigation.module.css';
+import classesGlobal from '../../global.module.css';
 
 
 const Navigation = (props) =>{
+  const getLinkClasses = (theme) => {
+    return [classes.Link, theme === "dark" ? classesGlobal.navlinkInactiveDark : classesGlobal.navlinkInactiveLight].join(" ");
+  };
 
   return (
     <ThemeConsumer>
       {({theme, toggleTheme})=>(
         <nav className={classes.Navigation}>
         <ul>
-          <li><NavLink to="/" exact activeClassName={classes.Active} className={classes.Link}>Top</NavLink></li>
-          <li><NavLink to="/new" exact activeClassName={classes.Active} className={classes.Link}>New</NavLink></li>
+          <li><NavLink to="/" exact activeClassName={classes.Active} className={getLinkClasses(theme)}>Top</NavLink></li>
+          <li><NavLink to="/new" exact activeClassName={classes.Active} className={getLinkClasses(theme)}>New</NavLink></li>
         </ul>
         <button onClick={()=>toggleTheme()}>{theme==="light" ? '🔦' : '💡'}</button>
       </nav>
